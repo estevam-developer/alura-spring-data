d94f928d86a801eb13a9c3a5ab890b4ad12c7401
Leonardo Alessandro Estevam <estevam.developer@gmail.com>
2022-05-30 15:50:02 -0300

    Commit inicial.

---
+++ src/main/java/br/com/estevam/spring/data/SpringDataApplication.java
@@ -0,0 +1,13 @@
+package br.com.estevam.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+}
